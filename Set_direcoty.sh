498d3cd1d587241d31ed83e34612a72484358850
HKyleZhang <hongkaizhang92@gmail.com>
2019-03-25 14:20:40 +0100

    modified

--- Set_direcoty.sh
+++ Set_direcoty.sh
@@ -17,8 +17,6 @@ echo "export PATH=\"${dir}/iqtree:\$PATH\"" >>~/.bashrc
 
 echo "export PATH=\"${dir}/igv:\$PATH\"" >>~/.bashrc
 
-echo "export PATH=\"${dir}/Mesquite_Folder:\$PATH\"" >>~/.bashrc
-
 echo "export PATH=\"${dir}/paup:\$PATH\"" >>~/.bashrc
 echo "export PATH=\"${dir}/paml:\$PATH\"" >>~/.bashrc
 echo "export PATH=\"${dir}/prank:\$PATH\"" >>~/.bashrc
