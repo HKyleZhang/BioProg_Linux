668d79f2c225f9f38cd77f62dcfd7253f1be149c
HKyleZhang <hongkaizhang92@gmail.com>
2019-04-08 15:15:33 +0200

    update

--- Set_direcoty.sh
+++ Set_direcoty.sh
@@ -37,7 +37,7 @@ echo "export PATH=\"${dir}/prank:\$PATH\"" >>~/.bashrc
 
 echo "export PATH=\"${dir}/raxml:\$PATH\"" >>~/.bashrc
 
-echo "export PATH=\"${dir}/samtools/bin:\$PATH\"" >>~/.bashrc
+echo "export PATH=\"${dir}/samtools:\$PATH\"" >>~/.bashrc
 
 echo "export PATH=\"${dir}/tablet:\$PATH\"" >>~/.bashrc
 echo "export PATH=\"${dir}/t_coffee/bin:\$PATH\"" >>~/.bashrc
